29c3f8567508c0e0db9528403ac665f168feb0e2
Maria Gryn <maria.grin2003@gmail.com>
2023-04-15 20:14:07 +0300

    setting up context for theme(light/dark)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import './App.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "@/components/Header/Header";
+import { useState, createContext } from "react"
+
+export const ThemeContext = createContext<{ theme: string, changeTheme: () => void }>({
+    theme: "light",
+    changeTheme: () => {}
+})
 
 function App() {
-  return (
-      <div className="page">
-          <Header/>
-      </div>
-  )
+    const [theme, setTheme] = useState<string>("light")
+
+    const changeTheme = () => {
+        setTheme(state => state === "light" ? "dark" : "light")
+    }
+
+    return (
+        <ThemeContext.Provider value={{theme, changeTheme}}>
+            <div className="page" id={theme}>
+                <Header/>
+            </div>
+        </ThemeContext.Provider>
+    )
 }
 
 export default App
