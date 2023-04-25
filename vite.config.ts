bba42f96d7d8f5d559b1ff86fa98fb8badce4cdc
Maria Gryn <maria.grin2003@gmail.com>
2023-04-25 18:47:49 +0300

    Adding dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/om-it-solutions/',
+  base: '/om-it-solutions-test-task/',
   plugins: [react()],
   resolve: {
     alias: [
